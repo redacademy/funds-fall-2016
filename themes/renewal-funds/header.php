707be23df723fca5e9c6418fdbdb48639f743b5e
martinz <martinzhangyx@gmail.com>
2016-11-29 14:17:49 -0800

    set header padding and icon size

--- themes/renewal-funds/header.php
+++ themes/renewal-funds/header.php
@@ -28,7 +28,7 @@
 				</div><!-- .site-branding -->
 
 				<div class="nav-btn">
-					<i class="fa fa-bars" aria-hidden="true"></i>
+					<i class="fa fa-bars fa-2x" aria-hidden="true"></i>
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
--- themes/renewal-funds/js/renewal-funds.js
+++ themes/renewal-funds/js/renewal-funds.js
@@ -1,1 +1,8 @@
 // js file for renewal funds
+(function( $ ) {
+    $('.main-navigation').hide();
+    $('.fa').on('click',function(event){
+        event.preventDefault();
+        $()
+    });
+})(jQuery);
