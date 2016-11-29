3f188ba260b997dfa4d01b4aac388fbbc8a85df4
martinz <martinzhangyx@gmail.com>
2016-11-29 14:25:34 -0800

    fix header structure

--- themes/renewal-funds/header.php
+++ themes/renewal-funds/header.php
@@ -21,14 +21,18 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<!--<p class="site-description"><?php bloginfo( 'description' ); ?></p>-->
-					<img src="<?php echo get_template_directory_uri().'/images/logos/' ?>" class= "header-logo" alt="header-logo" />
-				</div><!-- .site-branding -->
-
-				<div class="nav-btn">
-					<i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+				<div class="header-bar">
+					<div class="site-branding">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<!--<p class="site-description"><?php bloginfo( 'description' ); ?></p>-->
+						<img src="<?php echo get_template_directory_uri().'/images/logos/' ?>" class= "header-logo" alt="header-logo" />
+					</div><!-- .site-branding -->
+					<div class="nav-btn">
+						<i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+					</div>
+				</div>
+				
+				<div class="nav-option">
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
