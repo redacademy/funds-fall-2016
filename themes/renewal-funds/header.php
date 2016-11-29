170c12df46c926d117c6f819f685959214182753
martinz <martinzhangyx@gmail.com>
2016-11-29 14:05:01 -0800

    fix gulp/add header.scss/add navi-btn

--- themes/renewal-funds/header.php
+++ themes/renewal-funds/header.php
@@ -27,10 +27,13 @@
 					<img src="<?php echo get_template_directory_uri().'/images/logos/' ?>" class= "header-logo" alt="header-logo" />
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				<div class="nav-btn">
+					<i class="fa fa-bars" aria-hidden="true"></i>
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
