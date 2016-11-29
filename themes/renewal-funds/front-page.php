7668d39e788bc533e4eac95f1ea51584c43c678c
justinl-y <j@solaris.org.uk>
2016-11-29 06:09:53 -0800

    Front-page.php updated with login logic

--- themes/renewal-funds/front-page.php
+++ themes/renewal-funds/front-page.php
@@ -9,30 +9,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-        <p>front-page.php</p>
+        	<p>front-page.php</p>
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content' ); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+			<?php 
+				// conditional logic to migrate to custom teplate part on user role
+				if( current_user_can( 'administrator' ) ):
+					get_template_part( 'template-parts/content', 'renewal-fund' );
+				elseif ( current_user_can( 'editor') ) :
+					get_template_part( 'template-parts/content', 'portfolio-company' );
+				endif; 
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
