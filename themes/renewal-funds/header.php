4e08585cb5ee2cbebfee8b822cc2811b4bae68be
martinz <martinzhangyx@gmail.com>
2016-11-29 18:04:00 -0800

    make navi full screen

--- themes/renewal-funds/header.php
+++ themes/renewal-funds/header.php
@@ -33,6 +33,13 @@
 					<div class="nav-logo">
 						<img src="<?php echo get_template_directory_uri().'/images/logos/' ?>" class= "header-logo-bigger" alt="header-logo-bigger" />
 					</div>
+					<div class="nav-btn2">
+						<i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+					</div>
+					<nav id="site-navigation" class="secondery-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav>
 				</div>
 
 				<div class="nav-option">
--- themes/renewal-funds/js/renewal-funds.js
+++ themes/renewal-funds/js/renewal-funds.js
@@ -1,19 +1,16 @@
 // js file for renewal funds
 (function( $ ) {
-    $('.main-navigation').hide();
     $('.nav-logo').hide();
+    $('.main-navigation').hide();
     $('.nav-btn').on('click',function(event){
         event.preventDefault();
         $('.site-header').addClass('header-resize');
-        $('.site-branding').hide();
-        $('.nav-btn').hide();
         $('.nav-logo').show();
         $('.main-navigation').show();
     });
-    $('.site-header').on('mouseleave', function(){
+    $('.nav-btn2').on('click', function(event){
+        event.preventDefault();
         $('.site-header').removeClass('header-resize');
-        $('.site-branding').show();
-        $('.nav-btn').show();
         $('.nav-logo').hide();
         $('.main-navigation').hide();
     })
