c448d6cfbbb9d70e5ee8381c96ce8d4b5dc8eea0
Cheol Kim <kr.ckim91@gmail.com>
2016-11-30 09:02:54 -0800

    added media query

--- themes/renewal-funds/template-parts/content-renewal-fund.php
+++ themes/renewal-funds/template-parts/content-renewal-fund.php
@@ -1,3 +1,43 @@
-<p>content-renewal-fund.php</p>
+<?php
+get_header(); ?>
 
-<h1>changing something in content-renewal-fun.php</h1>
+<div class="welcome-wrapper">
+    <div class="welcome">
+        <p>Welcome Back,</p>
+        <span class="green-text"><p>Laura</p></span>
+    </div>
+</div>
+
+<div class="main-nav-wrapper">
+    <div class="main-nav">
+
+        <div class="nav-icon-wrapper">
+            <a href ="#"><div class="nav-icon">
+                <img src=""/>
+            </div></a>
+            <p>Portfolio Companies</p>
+        </div>
+
+        <div class="nav-icon-wrapper">
+            <a href ="#"><div class="nav-icon">
+                <img src=""/>
+            </div></a>
+            <p>Create Questionnaire</p>
+        </div>
+
+        <div class="nav-icon-wrapper">
+            <a href ="#"><div class="nav-icon">
+                <img src=""/>
+            </div></a>
+            <p>View Responses</p>
+        </div>
+
+        <div class="nav-icon-wrapper">
+            <a href ="#"><div class="nav-icon">
+                <img src=""/>
+            </div></a>
+            <p>Set Reminders</p>
+        </div>
+
+    </div>
+</div>
