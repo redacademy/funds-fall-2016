f450e2aacdf8cf3904ce683c90ca22e93038bbcf
Cheol Kim <kr.ckim91@gmail.com>
2016-12-01 10:12:14 -0800

    added correct images for dashboard/feedback page

--- themes/renewal-funds/template-parts/content-portfolio-company.php
+++ themes/renewal-funds/template-parts/content-portfolio-company.php
@@ -5,57 +5,64 @@
 get_header(); ?>
 <section class="container">
 
-<div class="welcome-wrapper">
-    <div class="welcome">
-        <p>Welcome Back,</p>
-        <span class="green-text"><p>Company</p></span>
-    </div>
-</div>
-
-<div class="main-nav-wrapper">
-    <div class="main-dashboard-nav">
-
-        <div class="nav-icon-wrapper">
-            <a href ="#"><div class="nav-icon">
-                <img src="<?php echo get_template_directory_uri()?>../assets/icons/add_portfolio_company_icon.png" alt="Add portfolio logo">
-            </div></a>
-            <p>Share your Story</p>
-        </div>
-
-        <div class="nav-icon-wrapper">
-            <a href ="#"><div class="nav-icon">
-                <img src="<?php echo get_template_directory_uri()?>../assets/icons/fill_questionnaire_icon.png" alt="Fill Quest logo">
-            </div></a>
-            <p>Respond Questionnaire</p>
-        </div>
-
-        <div class="nav-icon-wrapper">
-            <a href ="#"><div class="nav-icon">
-                <img class="history-logo" src="<?php echo get_template_directory_uri()?>../assets/icons/history_icon.png" alt="History logo">
-            </div></a>
-            <p>View History</p>
-        </div>
-
-        <div class="nav-icon-wrapper">
-            <a href ="#"><div class="nav-icon">
-                <img src="<?php echo get_template_directory_uri()?>../assets/icons/contact_icon.png" alt="Contact logo">
-            </div></a>
-            <p>Feedback</p>
-        </div>
-
-    </div>
-</div>
-
-<section class="trademark-section">
-
-    <div class="trademark-image"></div>
-
-    <div class="trademark-wrapper">
-        <div class="trademark-logo"></div>
-        <div class="trademark-text">
-            <p>We want to help tell your story, showing the difference our partners make in the world is important to us. Here, you will be able to respond to questionnaires, edit your profile, view your history, share your story, and stay in touch with us.</p>
+    <div class="welcome-wrapper">
+        <div class="welcome">
+            <p>Welcome Back,</p>
+            <span class="green-text"><p>Company</p></span>
         </div>
     </div>
-</section>
+
+    <div class="main-nav-wrapper">
+        <div class="main-dashboard-nav">
+
+            <div class="nav-icon-wrapper">
+                <a href ="#"><div class="nav-icon">
+                    <img src="<?php echo get_template_directory_uri()?>../assets/icons/add_portfolio_company_icon.png" alt="Add portfolio logo">
+                </div></a>
+                <p>Share your Story</p>
+            </div>
+
+            <div class="nav-icon-wrapper">
+                <a href ="#"><div class="nav-icon">
+                    <img src="<?php echo get_template_directory_uri()?>../assets/icons/fill_questionnaire_icon.png" alt="Fill Quest logo">
+                </div></a>
+                <p>Respond Questionnaire</p>
+            </div>
+
+            <div class="nav-icon-wrapper">
+                <a href ="#"><div class="nav-icon">
+                    <img class="history-logo" src="<?php echo get_template_directory_uri()?>../assets/icons/history_icon.png" alt="History logo">
+                </div></a>
+                <p>View History</p>
+            </div>
+
+            <div class="nav-icon-wrapper">
+                <a href ="#"><div class="nav-icon">
+                    <img src="<?php echo get_template_directory_uri()?>../assets/icons/contact_icon.png" alt="Contact logo">
+                </div></a>
+                <p>Feedback</p>
+            </div>
+
+        </div>
+    </div>
+
+<!--Trademark Section only in desktop-->
+
+    <section class="trademark-section">
+
+        <div class="trademark-image-wrapper">
+            <img class="trademark-image" src="<?php echo get_template_directory_uri()?>../assets/images/Dashboard_BG.png" alt="Renewal Slogan logo">
+        </div>
+
+        <div class="trademark-wrapper">
+            <div class="trademark-logo-wrapper">
+                <img class="trademark-logo" src="<?php echo get_template_directory_uri()?>../assets/icons/renewalfunds_slogan.png" alt="Renewal Slogan logo">
+            </div>
+            <div class="trademark-text">
+                <p>We want to help tell your story, showing the difference our partners make in the world is important to us. Here, you will be able to respond to questionnaires, edit your profile, view your history, share your story, and stay in touch with us.</p>
+            </div>
+        </div>
+
+    </section>
 
 </section>
