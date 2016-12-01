ca51f8f6f3c4f224a8d32d7ab8cf34fc54d3d71c
Cheol Kim <kr.ckim91@gmail.com>
2016-11-30 17:12:46 -0800

    styled feedback form/page

---
+++ themes/renewal-funds/feedback.php
@@ -0,0 +1,24 @@
+
+<?php /* Template Name: Feedback Page */ ?>
+<?php get_header(); ?>
+<div id="primary-feedback" class="content-area-feedback">
+ <main id="main" class="site-main" role="main">
+
+     <p>feedback.php</p>
+     <div class="feedback-page-wrapper">
+         
+        <div class="feedback-image-wrapper">
+            <div class="feedback-image"
+            style="background-image: url('<?php echo CFS()->get('feedback_image');?>')">
+            </div>
+        </div>
+
+        <div class="feedback-form-wrapper">
+            <?php echo CFS()->get('feedback_form');?>
+        </div>
+
+     </div>
+     
+ </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
