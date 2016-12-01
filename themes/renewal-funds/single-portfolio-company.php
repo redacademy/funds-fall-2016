cec0a19d526cdb88248a11471ac042cbf61af686
justinl-y <j@solaris.org.uk>
2016-12-01 09:18:53 -0800

    portfolio-company.php updated

--- themes/renewal-funds/single-portfolio-company.php
+++ themes/renewal-funds/single-portfolio-company.php
@@ -11,18 +11,19 @@ get_header(); ?>
 			<p>single-portfolio-company.php</p>
 
 			<?php  
-			
-			$pc_fund_name = '';//mega fund';
-			$pc_contact_name = '';//great contact';
-			$pc_contact_email = '';//great.contact@bob.com';
-			
-			gravity_form( $id_or_title = 1, 
-										$display_title = true, 
-										$display_description = false, 
-										$display_inactive = true, 
-										$field_values = array( 'pc_fund' => $pc_fund_name, 'pc_contact_name' => $pc_contact_name, 'pc_contact_email' => $pc_contact_email ), 
-										$ajax = false, 
-										$echo = false ); ?>
+				echo the_title();
+
+				$pc_fund_name = '';//mega fund';
+				$pc_contact_name = '';//great contact';
+				$pc_contact_email = '';//great.contact@bob.com';
+				
+				gravity_form( $id_or_title = 1, 
+											$display_title = true, 
+											$display_description = false, 
+											$display_inactive = true, 
+											$field_values = array( 'pc_fund' => $pc_fund_name, 'pc_contact_name' => $pc_contact_name, 'pc_contact_email' => $pc_contact_email ), 
+											$ajax = false, 
+											$echo = false ); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
