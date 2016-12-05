96cbb0a069d07dd3de005a013246258ac5a3df55
justinl-y <j@solaris.org.uk>
2016-12-05 14:53:10 -0800

    Questionnaire pages modified

--- themes/renewal-funds/single-questionnaire.php
+++ themes/renewal-funds/single-questionnaire.php
@@ -9,6 +9,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+
+			
+
+
+
 		<?php while ( have_posts() ) : the_post(); ?>
 			<p>single-questionnaire.php</p>
 
