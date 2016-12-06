4de5198abe9d008984ff5085ae7ee1bb3590818a
Cheol Kim <kr.ckim91@gmail.com>
2016-12-06 13:42:25 -0800

    styled history page and single story page

--- themes/renewal-funds/single-story.php
+++ themes/renewal-funds/single-story.php
@@ -10,13 +10,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<p>single-story.php</p>
+
+			<div class="single-story-wrapper container">
+			<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+			<div class="single-image" style="background-image: url('<?php echo $thumb['0'];?>')">
+			</div>
+			
         
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
+				<div class="single-story-text">
 				<?php the_field('story_body'); ?>
-				<?php the_post_navigation(); ?>
+				</div>
 			<?php endwhile; // End of the loop. ?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
