7e5a358b0893a465c8ab639d60b4815670682e45
Cheol Kim <kr.ckim91@gmail.com>
2016-12-11 13:21:06 -0800

    added padding wrap on history page

--- themes/renewal-funds/archive-portfolio-company.php
+++ themes/renewal-funds/archive-portfolio-company.php
@@ -7,7 +7,6 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-	<p>archive-portfolio-company.php</p>
 
 	<!--portfolio company year -->
 	<div class="history-page-wrapper wrap container">
