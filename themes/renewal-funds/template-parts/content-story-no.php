736a56b53569d28d2bb0af27f38324f7fe4e6b48
Cheol Kim <kr.ckim91@gmail.com>
2016-12-11 17:47:33 -0800

    fixed little padding issue on header of feedback

--- themes/renewal-funds/template-parts/content-story-no.php
+++ themes/renewal-funds/template-parts/content-story-no.php
@@ -1,25 +1,25 @@
 <section>
 	<div class="page-content">
-    <div class="thanks-page-wrapper container wrap">
+        <div class="thanks-page-wrapper container wrap">
 
-        <div class="error-header">
-            <h1>Hmmm...</h1>
-            <span class="error-header-message"><p>seems like there's no stories yet</p></span>
+            <div class="error-header">
+                <h1>Hmmm...</h1>
+                <span class="error-header-message"><p>seems like there's no stories yet</p></span>
+            </div>
+
+            <div class="thanks-image-wrapper">
+                <img src="<?php echo get_template_directory_uri()?>/assets/icons/png/create_questionnaire_icon.png" alt="create questionnaire logo">
+            </div>
+
+            <div class="error-header-message">
+                <p>Showing the difference our partners make in the world is important to us.</p>
+            </div>
+
+            <div class="home-button">
+                <a href="<?php echo get_page_link( get_page_by_title('Story Upload')->ID ); ?>">Add Story</a>
+            </div>
+
         </div>
-
-        <div class="thanks-image-wrapper">
-            <img src="<?php echo get_template_directory_uri()?>/assets/icons/png/create_questionnaire_icon.png" alt="create questionnaire logo">
-        </div>
-
-        <div class="error-header-message">
-            <p>Showing the difference our partners make in the world is important to us.</p>
-        </div>
-
-        <div class="home-button">
-            <a href="<?php echo get_page_link( get_page_by_title('Story Upload')->ID ); ?>">Add Story</a>
-        </div>
-
-    </div>
     </div><!-- .page-content -->
 </section>
 
