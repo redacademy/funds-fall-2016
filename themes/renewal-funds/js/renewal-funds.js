709c42975c6feb71a2e069ef734449b816c70867
martinz <martinzhangyx@gmail.com>
2016-12-13 11:24:12 -0800

    clear code

--- themes/renewal-funds/js/renewal-funds.js
+++ themes/renewal-funds/js/renewal-funds.js
@@ -6,9 +6,8 @@
         event.preventDefault();
         $( '.site-header' ).addClass( 'header-resize' );
         $( '.menu-toggle' ).hide();
-        var height = $(window).height();
-        var scrollbarHeight = height - 200; 
-        $('.header-resize .main-navigation .menu').css('max-height', scrollbarHeight);
+        var height = $(window).height(); 
+        $('.header-resize .main-navigation .menu').css('max-height', height-200);
     });
 
     $( '.nav-cancel' ).on( 'click', function( event ){
@@ -18,30 +17,8 @@
 
     $(window).resize(function(){
         $( '.site-header' ).removeClass( 'header-resize' );
-        var scrollbarHeight = $(window).height() - 200;
     });
 
-    $( '.acf-input-wrap input' ).prop( 'disabled', true ); 
-    //$( '.acf-form' ).css( 'background-color: white'); 
-
-    $('.page-template-feedback .gform_button').on('click',function(event){
-        var content = $('.textarea').val().length;
-        if (content === 0 ){
-            $('.page-template-feedback .textarea.medium').addClass('invalid');
-            if (('#input_2_1').hasClass('invalid')){
-                $('.page-template-feedback .gform_button').preventDefault();
-            }
-        } else {
-            $('.page-template-feedback .textarea.medium').removeClass('invalid');
-        }
-    });
-
-    $('.page-template-feedback .gform_button').on('click',function(event){
-        
-    });
-    
-    // if($('#target').hasClass('is-selected')) {
-
-    // }
+    $( '.acf-input-wrap input' ).prop( 'disabled', true );  
 
 })(jQuery);
